0e0c5842a3d6c320d400167d1c40ab9d26593197
Gaditek <Gaditek@DESKTOP-HURV49N>
2017-05-26 19:50:18 +0500

    commit

---
+++ src/pro4jenkins/myclass.java
@@ -0,0 +1,12 @@
+package pro4jenkins;
+
+public class myclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world");
+
+	}
+
+}
