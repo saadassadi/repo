d5bba2678fd97892dcef8a378c4767b5f03db37d
Gaditek <Gaditek@DESKTOP-HURV49N>
2017-05-29 12:28:00 +0500

    commit

--- src/pro4jenkins/myclass.java
+++ src/pro4jenkins/myclass.java
@@ -5,9 +5,8 @@ public class myclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello world");
-		System.out.println("hello world twice");
-		System.out.println("hello world three");
+		System.out.println("hello world ");
+
 
 	}
 
