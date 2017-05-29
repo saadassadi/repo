4069cb2487d34595f674a423831d41079998ac9e
Gaditek <Gaditek@DESKTOP-HURV49N>
2017-05-29 12:30:25 +0500

    last commit

--- src/pro4jenkins/myclass.java
+++ src/pro4jenkins/myclass.java
@@ -5,7 +5,7 @@ public class myclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello world ");
+		System.out.println("change in code on every build ");
 
 
 	}
